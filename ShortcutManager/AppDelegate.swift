6f08a64389e975f44db847d4101a3f79743e320f
bugkingK <myway0710@naver.com>
2019-06-20 00:10:19 +0900

    Initial Commit

---
+++ ShortcutManager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ShortcutManager
+//
+//  Created by bugking on 20/06/2019.
+//  Copyright © 2019 bugking. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ShortcutManager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ShortcutManager
+//
+//  Created by bugking on 20/06/2019.
+//  Copyright © 2019 bugking. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

4e4583dc4115b709d9593354c8509516e0694a53
bugkingK <myway0710@naver.com>
2019-06-20 12:31:39 +0900

    by moon : start popover

--- ShortcutManager/ViewControllers/AppDelegate.swift
+++ ShortcutManager/ViewControllers/AppDelegate.swift
@@ -15,10 +15,16 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        PopoverController.sharedInstance()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+    }
+    
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        sender.windows.first?.setIsVisible(true)
+        return true
     }
 
 
